c48ee7a156cd45b20c156427a77bd82d1369b727
brandong1 <brandon@brandonleeproductions.com>
2019-03-05 23:07:25 -0700

    add array syntax

---
+++ arrays.swift
@@ -0,0 +1,8 @@
+//ARRAYS
+
+var evenNumbers = [2, 4, 6, 8]
+var songs = ["Shake it off", "You belong with me", "Back to December"]
+
+//Arrays in Swift work the same in Ruby; they start at index 0.
+
+songs[0] equals "Shake it off"
