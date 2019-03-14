4dee0cbdd9b96537b01b9448f8ee68e58338b836
brandong1 <brandon@brandonleeproductions.com>
2019-03-13 23:20:29 -0600

    add arrays with explicit data type

--- arrays.swift
+++ arrays.swift
@@ -5,4 +5,11 @@ var songs = ["Shake it off", "You belong with me", "Back to December"]
 
 //Arrays in Swift work the same in Ruby; they start at index 0.
 
-songs[0] equals "Shake it off"
+// songs[0] equals "Shake it off"
+
+// I'm telling Swift I want to store only strings in the array.
+var songs: [String] = ["Shake it off", "You Belong with Me", "Back to December"]
+
+// I'm tell Swift I want this array to hold any data type.
+var songs: [Any] = ["Shake it off", "You Belong with Me", "Back to December"]
+
